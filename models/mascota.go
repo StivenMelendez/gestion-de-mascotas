0d73d368dd4f9b86c603b2c4eed30d2b325cb4f3
StivenMelendez <stivenrafael@hotmail.com>
2025-04-25 22:10:54 -0500

    se corrigió el tipo de dato de la propiedad Foto en la estructura Mascota de 'image.Image' a 'string', y se añadieron archivos para la codificación y decodificación de imágenes

--- models/mascota.go
+++ models/mascota.go
@@ -1,21 +1,20 @@
 package models
 
 import (
-	"image"
 	"time"
 )
 
 type Mascota struct {
-	ID                uint        `bson:"id" json:"id"`
-	Foto              image.Image `bson:"foto" json:"foto"`
-	Nombre            string      `bson:"nombre" json:"nombre"`
-	RazaID            uint        `bson:"raza_id" json:"raza_id"`
-	Peso              float64     `bson:"peso" json:"peso"`
-	DuenoID           uint        `bson:"dueno_id" json:"dueno_id"`
-	FechaDeNacimiento time.Time   `bson:"fecha_de_nacimiento" json:"fecha_de_nacimiento"`
-	CreatedAt         time.Time   `bson:"created_at" json:"created_at"`
-	UpdatedAt         time.Time   `bson:"updated_at" json:"updated_at"`
-	DeletedAt         time.Time   `bson:"deleted_at,omitempty" json:"deleted_at,omitempty"`
+	ID                uint      `bson:"id" json:"id"`
+	Foto              string    `bson:"foto" json:"foto"`
+	Nombre            string    `bson:"nombre" json:"nombre"`
+	RazaID            uint      `bson:"raza_id" json:"raza_id"`
+	Peso              float64   `bson:"peso" json:"peso"`
+	DuenoID           uint      `bson:"dueno_id" json:"dueno_id"`
+	FechaDeNacimiento time.Time `bson:"fecha_de_nacimiento" json:"fecha_de_nacimiento"`
+	CreatedAt         time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt         time.Time `bson:"updated_at" json:"updated_at"`
+	DeletedAt         time.Time `bson:"deleted_at,omitempty" json:"deleted_at,omitempty"`
 }
 
 type Mascotas []Mascota
